e1b78ff3df3890c51112dfc56a78355bff0f8d37
mmpotulo28 <mpotulom28@gmail.com>
2024-12-18 12:23:39 +0200

    initialize project

---
+++ src/main/java/org/example/sprintscoreapi/SprintScoreApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.sprintscoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintScoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintScoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sprintscoreapi/SprintScoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sprintscoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintScoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
